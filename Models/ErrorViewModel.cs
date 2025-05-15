245ea36075c6ac3b36c5ad62c743ad5f2d2ec0eb
FRANCO URQUIZO BARBA <49257286@ort>
2025-05-15 08:49:26 -0300

    ProyectoCreado

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP04_Urquizo_Brasburg.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
